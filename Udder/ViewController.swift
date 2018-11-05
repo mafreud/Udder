2d5f7f0dcca195197a93100c49515560c6baef38
Masahiro Aoki <rafe.kun@gmail.com>
2018-11-05 19:25:07 +0900

    initial commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by AOKI, Masahiro on 2018/11/05.
+//  Copyright © 2018 WoundedHealer, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
